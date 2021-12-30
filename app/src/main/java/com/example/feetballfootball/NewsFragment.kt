fe077e2c6354405fff102ea6cbe7b5b6050e46ac
Jongwoo Kim <kjwoo810@gmail.com>
2021-12-30 21:28:16 +0900

    temporary add picture(barricade) in NewsFragment

--- app/src/main/java/com/example/feetballfootball/NewsFragment.kt
+++ app/src/main/java/com/example/feetballfootball/NewsFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 
 
+private const val TAG = "NewsFragment"
 class NewsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
