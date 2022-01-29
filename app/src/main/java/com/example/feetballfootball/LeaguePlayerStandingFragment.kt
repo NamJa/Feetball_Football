2aca4e70c7d76d20939c27a3c9056c702782417e
Jongwoo Kim <kjwoo810@gmail.com>
2022-01-30 02:12:26 +0900

    Prepare for Add PlayerStanding By League with TabLayout(Seperating LeagueStandingFragment)

---
+++ app/src/main/java/com/example/feetballfootball/LeaguePlayerStandingFragment.kt
@@ -0,0 +1,27 @@
+package com.example.feetballfootball
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class LeaguePlayerStandingFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_league_player_standing, container, false)
+        return view
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = LeaguePlayerStandingFragment()
+    }
+}
