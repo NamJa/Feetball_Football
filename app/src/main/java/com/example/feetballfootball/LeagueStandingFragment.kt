7e6ceba62e7a0156dfcf65f47ce366dbc9e9cab6
Jongwoo Kim <kjwoo810@gmail.com>
2022-02-02 02:44:02 +0900

    Complete Showing Player Standing(scorer, Assist) data

--- app/src/main/java/com/example/feetballfootball/LeagueStandingFragment.kt
+++ app/src/main/java/com/example/feetballfootball/LeagueStandingFragment.kt
@@ -32,7 +32,6 @@ private const val ARG_LEAGUE_ID = "league_id"
 class LeagueStandingFragment : Fragment() {
     private var leagueId: Int? = 0
     private var leagueCodeMap = mapOf(39 to "Premier League", 140 to "LA LIGA", 135 to "SERIE A", 78 to "BUNDESLIGA", 61 to "LIGUE 1")
-    private lateinit var standingLiveData : LiveData<List<Standings>>
     private val tabTexts: List<String> = listOf("팀 순위", "개인 순위")
 
     private lateinit var leagueTitle : TextView
@@ -40,14 +39,12 @@ class LeagueStandingFragment : Fragment() {
     private lateinit var viewPager: ViewPager2
 
     private lateinit var standingViewModel: StandingViewModel
-//    private lateinit var leagueStandingRecyclerview: RecyclerView
 
     private var footballDataFetchr = FootballDataFetchr()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         leagueId = arguments?.getInt(ARG_LEAGUE_ID)
         standingViewModel = ViewModelProvider(this).get(StandingViewModel::class.java)
-//        standingLiveData = standingViewModel.fetchStadingLiveData(leagueId!!)
         footballDataFetchr.fetchPlayerScorerData(39, 2021)
         footballDataFetchr.fetchPlayerAssistData(39, 2021)
     }
@@ -78,14 +75,6 @@ class LeagueStandingFragment : Fragment() {
             override fun onTabReselected(tab: TabLayout.Tab?) {
             }
         })
-//        leagueStandingRecyclerview.layoutManager = LinearLayoutManager(context)
-//
-//        standingLiveData.observe(
-//            viewLifecycleOwner,
-//            Observer {
-//                updateUI(it)
-//            }
-//        )
 
         return view
     }
@@ -93,84 +82,7 @@ class LeagueStandingFragment : Fragment() {
         leagueTitle = view.findViewById(R.id.league_title_textview)
         tabs = view.findViewById(R.id.tabLayout)
         viewPager = view.findViewById(R.id.viewPager)
-//        leagueStandingRecyclerview = view.findViewById(R.id.league_standing_recyclerview)
     }
-//
-//    private fun updateUI(data: List<Standings>) {
-//        val adapter = LeagueStandingRecyclerViewAdapter(data)
-//        leagueStandingRecyclerview.adapter = adapter
-//    }
-//
-//    private inner class StandItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//
-//        val posColor: TextView
-//        val rank: TextView
-//        val teamLogo: ImageView
-//        val teamName: TextView
-//        val played: TextView
-//        val win: TextView
-//        val draw: TextView
-//        val lose: TextView
-//        val goalDiff: TextView
-//        val points: TextView
-//
-//        init {
-//            posColor = itemView.findViewById(R.id.pos_color_textview)
-//            rank = itemView.findViewById(R.id.rank_textview)
-//            teamLogo = itemView.findViewById(R.id.team_logo)
-//            teamName = itemView.findViewById(R.id.team_name_textview)
-//            played = itemView.findViewById(R.id.played_textview)
-//            win = itemView.findViewById(R.id.win_textview)
-//            draw = itemView.findViewById(R.id.draw_textview)
-//            lose = itemView.findViewById(R.id.lose_textview)
-//            goalDiff = itemView.findViewById(R.id.goalDiff_textview)
-//            points = itemView.findViewById(R.id.points_textview)
-//        }
-//
-//        fun bindLogo(team: Team) {
-//            Picasso.get()
-//                .load(team.logo)
-//                .resize(100,100)
-//                .into(teamLogo)
-//        }
-//    }
-//
-//    private inner class LeagueStandingRecyclerViewAdapter(var data: List<Standings>) : RecyclerView.Adapter<StandItemHolder>() {
-//        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StandItemHolder {
-//            val view = layoutInflater.inflate(R.layout.standing_item, parent, false)
-//            return StandItemHolder(view)
-//        }
-//
-//        override fun onBindViewHolder(holder: StandItemHolder, position: Int) {
-//            data.get(position).description?.let { description ->
-//                if(description.startsWith('P')){
-//                    if(description.contains("Champ")) {
-//                        holder.posColor.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.teal_200))
-//                    } else if (description.contains("Europa")) {
-//                        holder.posColor.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.indigo_500))
-//                    } else {
-//                        holder.posColor.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.green_300))
-//                    }
-//                } else {
-//                    // 강등 시 색 지정
-//                    holder.posColor.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.red_600))
-//                }
-//            }
-//            holder.rank.text =      data.get(position).rank.toString()
-//            holder.bindLogo(data.get(position).team)
-//            holder.teamName.text =  data.get(position).team.name
-//            holder.played.text =    data.get(position).all.played.toString()
-//            holder.win.text =       data.get(position).all.win.toString()
-//            holder.draw.text =      data.get(position).all.draw.toString()
-//            holder.lose.text =      data.get(position).all.lose.toString()
-//            holder.goalDiff.text =  data.get(position).goalsDiff.toString()
-//            holder.points.text =    data.get(position).points.toString()
-//        }
-//
-//        override fun getItemCount(): Int {
-//            return data.size
-//        }
-//    }
 
 
     companion object {
@@ -187,6 +99,7 @@ class LeagueStandingFragment : Fragment() {
     }
 
 }
+
 
 class TwoPagerAdapter(
     fragmentActivity: FragmentActivity,
--- app/src/main/java/com/example/feetballfootball/StandingViewModel.kt
+++ app/src/main/java/com/example/feetballfootball/StandingViewModel.kt
@@ -1,26 +1,37 @@
 package com.example.feetballfootball
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.example.feetballfootball.api.leaguestanding.StandingResponse
 import com.example.feetballfootball.api.leaguestanding.Standings
+import com.example.feetballfootball.api.playerstanding.PlayerStandingStatistics
 import org.threeten.bp.LocalDate
 import org.threeten.bp.Year
 
 class StandingViewModel: ViewModel() {
-    val standingLiveData: LiveData<List<Standings>>
+    val clubStandingLiveData: LiveData<List<Standings>>
+    val playerTopScorerLiveData: LiveData<List<PlayerStandingStatistics>>
+    val playerTopAssistLiveData: LiveData<List<PlayerStandingStatistics>>
     private val footballDataFetchr = FootballDataFetchr()
 
     // 추춘제 리그 때문에 이렇게 연도 데이터 할당
-    val currentSeason = if (LocalDate.now().monthValue < 7) {
+    val currentSeason = if (LocalDate.now().monthValue < 8) {
         Year.now().minusYears(1).value
     } else { Year.now().value }
 
     init {
-        standingLiveData = fetchStadingLiveData(league = 39)
+        clubStandingLiveData = fetchStadingLiveData(league = 39)
+        playerTopScorerLiveData = fetchPlayerTopScorerLiveData(league = 39)
+        playerTopAssistLiveData = fetchPlayerTopAssistLiveData(league = 39)
     }
+
     fun fetchStadingLiveData(league: Int) : LiveData<List<Standings>> {
         return footballDataFetchr.fetchLeagueStandings(league, season = currentSeason)
+    }
+
+    fun fetchPlayerTopScorerLiveData(league: Int) : LiveData<List<PlayerStandingStatistics>> {
+        return footballDataFetchr.fetchPlayerScorerData(league, season = currentSeason)
+    }
+    fun fetchPlayerTopAssistLiveData(league: Int) : LiveData<List<PlayerStandingStatistics>> {
+        return footballDataFetchr.fetchPlayerAssistData(league, season = currentSeason)
     }
 }
