490dc56d657fde1178563da505f1a3b4a2a06c41
jongwoo <kjwoo810@gmail.com>
2026-02-28 13:23:39 +0900

    [feature/migrate-viewBinding] FixtureRecyclerViewAdapter, PlayerLineupAdapter ViewBinding 적용

--- app/src/main/java/com/example/feetballfootball/adapter/FixtureRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/feetballfootball/adapter/FixtureRecyclerViewAdapter.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.Dimension
 import androidx.recyclerview.widget.RecyclerView
@@ -13,6 +11,7 @@ import com.example.feetballfootball.R
 import com.example.feetballfootball.api.Fixture
 import com.example.feetballfootball.api.FixtureResponse
 import com.example.feetballfootball.api.Teams
+import com.example.feetballfootball.databinding.FixtureBinding
 import com.squareup.picasso.Picasso
 
 class FixtureRecyclerViewAdapter(var context: Context, var fixtureData: MutableList<FixtureResponse>): RecyclerView.Adapter<FixtureRecyclerViewAdapter.DataViewHolder>() {
@@ -29,34 +28,21 @@ class FixtureRecyclerViewAdapter(var context: Context, var fixtureData: MutableL
     //                          이 어댑터 클래스마저 FixtureFragment에서 구현하여 약간의 가독성을 포기하느냐는 나중에 테스트해볼 예정
 
 
-    inner class DataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+    inner class DataViewHolder(val binding: FixtureBinding) : RecyclerView.ViewHolder(binding.root), View.OnClickListener {
 
         private lateinit var fixture: Fixture
 
-        val elapsedTextView : TextView
-        val homeTeamTextView: TextView
-        val scoreOrTimeTextView: TextView
-        val awayTeamTextView: TextView
-        val homeTeamLogo: ImageView
-        val awayTeamLogo: ImageView
-
         init {
-            elapsedTextView = itemView.findViewById(R.id.elapsed)
-            homeTeamTextView = itemView.findViewById(R.id.team_home)
-            scoreOrTimeTextView = itemView.findViewById(R.id.score_or_time)
-            awayTeamTextView = itemView.findViewById(R.id.team_away)
-            homeTeamLogo = itemView.findViewById(R.id.team_home_logo)
-            awayTeamLogo = itemView.findViewById(R.id.team_away_logo)
-            itemView.setOnClickListener(this)
+            binding.root.setOnClickListener(this)
         }
 
         fun bindLogo(teams: Teams) {
             Picasso.get()
                 .load(teams.home.logoUrl)
-                .into(homeTeamLogo)
+                .into(binding.teamHomeLogo)
             Picasso.get()
                 .load(teams.away.logoUrl)
-                .into(awayTeamLogo)
+                .into(binding.teamAwayLogo)
         }
         fun bind(fixture: Fixture) {
             this.fixture = fixture
@@ -72,37 +58,37 @@ class FixtureRecyclerViewAdapter(var context: Context, var fixtureData: MutableL
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context).inflate(R.layout.fixture, parent, false)
-        return DataViewHolder(layoutInflater)
+        val binding = FixtureBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DataViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
         when(fixtureData.get(position).fixture.status.short) {
             //https://www.api-football.com/documentation-v3#operation/get-fixtures 상태 코드 확인
             "PST" -> {
-                holder.elapsedTextView.text = ""
-                holder.scoreOrTimeTextView.setTextSize(Dimension.SP, 15f);
-                holder.scoreOrTimeTextView.text = "연기됨"
+                holder.binding.elapsed.text = ""
+                holder.binding.scoreOrTime.setTextSize(Dimension.SP, 15f);
+                holder.binding.scoreOrTime.text = "연기됨"
             }
             "NS" -> {
-                holder.elapsedTextView.text = ""
+                holder.binding.elapsed.text = ""
                 var times = fixtureData.get(position).fixture.date.split("T")[1].split("+")[0].split(":")
-                holder.scoreOrTimeTextView.text = (times[0]+":"+times[1])
+                holder.binding.scoreOrTime.text = (times[0]+":"+times[1])
             }
             "CANC" -> {
-                holder.elapsedTextView.text = ""
-                holder.scoreOrTimeTextView.text = "취소됨"
+                holder.binding.elapsed.text = ""
+                holder.binding.scoreOrTime.text = "취소됨"
             }
             else -> {
-                holder.elapsedTextView.text = fixtureData.get(position).fixture.status.elapsed.toString()+"'"
+                holder.binding.elapsed.text = fixtureData.get(position).fixture.status.elapsed.toString()+"'"
                 var goals = fixtureData.get(position).goals
-                holder.scoreOrTimeTextView.text = (goals.home.toString() +"-"+goals.away.toString())
+                holder.binding.scoreOrTime.text = (goals.home.toString() +"-"+goals.away.toString())
             }
         }
         holder.bind(fixtureData.get(position).fixture)
         holder.bindLogo(fixtureData.get(position).teams)
-        holder.homeTeamTextView.text = fixtureData.get(position).teams.home.name
-        holder.awayTeamTextView.text = fixtureData.get(position).teams.away.name
+        holder.binding.teamHome.text = fixtureData.get(position).teams.home.name
+        holder.binding.teamAway.text = fixtureData.get(position).teams.away.name
 
     }
 
